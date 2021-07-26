d018667fa0bdaea5ecd909616331fd1374213efa
dana8 <w2008@e-mirim.hs.kr>
2021-07-26 13:34:11 +0900

    Edit MainActivity(Add Item) Commit

--- app/src/main/java/com/example/emirim_ex4_5/MainActivity.java
+++ app/src/main/java/com/example/emirim_ex4_5/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editItem = findViewById(R.id.edit_item);
         arrList = new ArrayList<>();
         ListView list1 = findViewById(R.id.list1);
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrList);
